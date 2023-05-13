b983c41c55f312bc3074dd159943922361eab118
griphaugland <hauglanden1@gmail.com>
2023-05-13 11:56:40 +0200

    Changed loader design as well as added function Sort option on recipe page

--- js/recipes.js
+++ js/recipes.js
@@ -1,39 +1,100 @@
 const main = document.querySelector('#recipes-main');
-const url = "https://gripdev.no/exam1/wp-json/wp/v2/posts?_embed=wp:featuredmedia"
+const url = "https://gripdev.no/exam1/wp-json/wp/v2/posts?_embed=wp:featuredmedia&per_page=100"
 const delay = 2000;
 const loader = document.getElementById("loader-recipes");
+const loadmoreSection = document.querySelector('.loadmore-section')
+const loadmoreBtn = document.querySelector('.loadmore-button')
+const alphabetSort = document.querySelector('#alphabetSort')
+const mostRecent = document.querySelector('#mostRecent')
+const oldest = document.querySelector('#oldest')
+let allRecipes = [];
 
-const getRecipesSlider = () => {
+const getRecipes = () => {
 fetch(url)
 .then(res => res.json())
 .then((data)=>{
     loader.classList.add("show");
     loader.classList.remove("hide");
-    renderAllRecipes(data)
+    allRecipes = data;
+    renderRecipes(data.slice(0, 10)); 
     loader.classList.add("hide");
     loader.classList.remove("show");
 })
-
 }
 
-function renderAllRecipes(data) {
-        main.innerHTML = data.map((ele, index, arr) => 
-        {
-            const {id, title, excerpt} = ele;
-            let media = ele._embedded["wp:featuredmedia"][0].source_url;
-            return `
-            <a href="/pages/specific.html?id=${id}" class="box-recipes">
-                <div class="image_wrapper">
-                 <img class="imageel" src="${media}">
-                </div>
-                <div class="title-container">
-                <h1 class="recipes-main">${title.rendered}</h1>
-                </div>
-              </a>`;
-        }
-        ).join("")
-        
-console.log(data)
+const renderRecipes = (recipes) => {
+    main.innerHTML = recipes.map((ele) => {
+      const { id, title, excerpt } = ele;
+      let media = ele._embedded["wp:featuredmedia"][0].source_url;
+      return `
+        <a href="/pages/specific.html?id=${id}" class="box-recipes">
+          <div class="time">15-30min</div>
+          <div class="image_wrapper">
+            <img class="imageel" src="${media}">
+          </div>
+          <div class="title-container">
+            <h1 class="recipes-main">${title.rendered}</h1>
+          </div>
+        </a>`;
+    }).join("");
+    if (allRecipes.length > 9) {
+        loadmoreSection.style.display = "flex";
+      } else {
+        loadmoreSection.style.display = "none";
+      }
 }
-  setTimeout(() => {getRecipesSlider()}, delay);
 
+const loadMoreRecipes = () => {
+    const remainingRecipes = allRecipes.slice(10); 
+    renderRecipes(allRecipes); 
+    loadmoreSection.style.display = "none";
+  };
+  
+  loadmoreBtn.addEventListener('click', loadMoreRecipes);
+  setTimeout(getRecipes, delay);
+
+  const SortRecipesAlphabet = () => {
+    const sortedRecipes = allRecipes.sort(function (a, b) {
+      if (a.title.rendered < b.title.rendered) {
+        return -1;
+      }
+      if (a.title.rendered > b.title.rendered) {
+        return 1;
+      }
+      return 0;
+    }).slice(0, 10);; 
+    renderRecipes(sortedRecipes);
+  };
+
+  const SortRecipesNewest = () => {
+    const sortedRecipes = allRecipes.sort(function (a, b) {
+      if (a.date < b.date) {
+        return -1;
+      }
+      if (a.date > b.date) {
+        return 1;
+      }
+      return 0;
+    }).slice(0, 10);; 
+    renderRecipes(sortedRecipes); 
+  };
+
+  const SortRecipesOldest = () => {
+    const sortedRecipes = allRecipes.sort(function (a, b) {
+      if (a.date > b.date) {
+        return -1;
+      }
+      if (a.date < b.date) {
+        return 1;
+      }
+      return 0;
+    }).slice(0, 10);; 
+    renderRecipes(sortedRecipes); 
+  };
+
+  alphabetSort.addEventListener('click', 
+  SortRecipesAlphabet)
+  mostRecent.addEventListener('click', 
+  SortRecipesNewest)
+  oldest.addEventListener('click', 
+  SortRecipesOldest)
--- js/specific.js
+++ js/specific.js
@@ -6,34 +6,28 @@ const delay = 1000;
 const loader = document.getElementById("loader");
 const main = document.querySelector('#specific')
 
-/* const renderRecipe = () => {
-      const data = res.json();
-      const recipe = data[id];
-      main.innerHTML = 
-      ` <h1 class="">${recipe.title} </h1>
-              </div>  
-              <div class="jacketdesccontainer"> 
-              <p class="jacketdesc">${recipe.excerpt.rendered}</p> 
-              </div>`
-  }
- */
-const getSingleRecipe = () => {
+
+const fetchRecipes = () => {
     fetch(url)
     .then(res => res.json())
     .then((data)=>{
         loader.classList.add("show");
         loader.classList.remove("hide");
-        const data = res.json();
-        const recipe = data[id];
+        getSingleRecipe(data[id]); 
+        console.log(data)
+        loader.classList.add("hide");
+        loader.classList.remove("show");
+    })
+}
+
+const getSingleRecipe = () => {
+  
         main.innerHTML = 
-        ` <h1 class="">${recipe.title} </h1>
+        `<h1 class="">${recipe.title}</h1>
                 </div>  
                 <div class="jacketdesccontainer"> 
                 <p class="jacketdesc">${recipe.excerpt.rendered}</p> 
                 </div>`
         loader.classList.add("hide");
         loader.classList.remove("show");
-    })
-    
     }
-    getSingleRecipe();
