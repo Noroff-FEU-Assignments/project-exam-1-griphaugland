c0489cebdd1dc06c87cce333323aeeee79665e18
griphaugland <hauglanden1@gmail.com>
2023-05-16 21:48:30 +0200

    Started to create recipe specific pages.

--- js/specific.js
+++ js/specific.js
@@ -1,11 +1,10 @@
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get('id');
-const url = 'https://gripdev.no/exam1/wp-json/wp/v2/posts?_embed=wp:featuredmedia';
+const url = 'https://gripdev.no/exam1/wp-json/wp/v2/posts?_embed=wp:featuredmedia&per_page=20';
 const delay = 1000;
-const loader = document.getElementById("loader");
-const main = document.querySelector('#specific')
-
+const loader = document.getElementById("loaderFullPage");
+const main = document.getElementById('specific');
 
 const fetchRecipes = () => {
     fetch(url)
@@ -13,21 +12,32 @@ const fetchRecipes = () => {
     .then((data)=>{
         loader.classList.add("show");
         loader.classList.remove("hide");
-        getSingleRecipe(data[id]); 
-        console.log(data)
+        renderSingleRecipe(data); 
         loader.classList.add("hide");
         loader.classList.remove("show");
     })
 }
 
-const getSingleRecipe = () => {
-  
-        main.innerHTML = 
-        `<h1 class="">${recipe.title}</h1>
-                </div>  
-                <div class="jacketdesccontainer"> 
-                <p class="jacketdesc">${recipe.excerpt.rendered}</p> 
-                </div>`
-        loader.classList.add("hide");
-        loader.classList.remove("show");
+
+function renderSingleRecipe(data) {
+    const recipe = data.find(post => post.id === parseInt(id));
+    let media = recipe._embedded["wp:featuredmedia"][0].source_url;
+            main.innerHTML = 
+        `<section class="shell">
+            <div class="top-specific">
+                <h1 class="title-spesific">${recipe.title.rendered}</h1>
+                <div class="time">15-20min</div>
+            </div>
+            <div class="top-middle-wrapper">
+             <div class="ingredients"></div>
+             <div class="specific-image"><img class="spec-img-el" src="${media}"></div>
+            </div>
+            <div class="preparation">
+                <h3 class="prep-title">Tilberedning</h3>
+                ${recipe.content.rendered}
+            </div>
+                
+         </section>        
+                `   
     }
+    fetchRecipes();
